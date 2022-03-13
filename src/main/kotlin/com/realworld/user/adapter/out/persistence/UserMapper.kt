5907bcf19ee6aa611cb2c719f96d4f6049e1e927
WonJongWoon <delphi3228@pukyong.ac.kr>
2022-03-13 17:08:33 +0900

    feat: Persistence User Entity

--- src/main/kotlin/com/realworld/user/adapter/out/persistence/UserMapper.kt
+++ src/main/kotlin/com/realworld/user/adapter/out/persistence/UserMapper.kt
@@ -1,9 +1,19 @@
 package com.realworld.user.adapter.out.persistence
 
 import com.realworld.user.application.port.`in`.RegistrationCommand
+import com.realworld.user.domain.User
+import org.apache.logging.log4j.util.Strings
+import org.springframework.security.crypto.password.PasswordEncoder
+import org.springframework.stereotype.Component
 
-class UserMapper {
-    fun mapToUser(registrationCommand: RegistrationCommand) {
-        TODO()
-    }
+@Component
+class UserMapper(private val passwordEncoder: PasswordEncoder) {
+
+    fun mapToUser(registrationCommand: RegistrationCommand) = User(
+        registrationCommand.username,
+        registrationCommand.email,
+        passwordEncoder.encode(registrationCommand.password),
+        Strings.EMPTY,
+        Strings.EMPTY
+    )
 }
