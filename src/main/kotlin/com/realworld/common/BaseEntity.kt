12868fab8e2fecee5365fd3194180521a889d8bf
WonJongWoon <delphi3228@pukyong.ac.kr>
2022-03-13 16:27:48 +0900

    feat: Add BaseEntity, Enable JpaAuditing

---
+++ src/main/kotlin/com/realworld/common/BaseEntity.kt
@@ -0,0 +1,32 @@
+package com.realworld.common
+
+import org.springframework.data.annotation.CreatedDate
+import org.springframework.data.annotation.LastModifiedDate
+import org.springframework.data.jpa.domain.support.AuditingEntityListener
+import java.io.Serializable
+import java.time.LocalDateTime
+import javax.persistence.Column
+import javax.persistence.EntityListeners
+import javax.persistence.GeneratedValue
+import javax.persistence.GenerationType
+import javax.persistence.Id
+import javax.persistence.MappedSuperclass
+
+@EntityListeners(AuditingEntityListener::class)
+@MappedSuperclass
+abstract class BaseEntity : Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    open var id: Long? = lateInit()
+
+    @CreatedDate
+    @Column(updatable = false)
+    open val createdAt: LocalDateTime = lateInit()
+
+    @LastModifiedDate
+    open val modifiedAt: LocalDateTime = lateInit()
+}
+
+@Suppress("UNCHECKED_CAST")
+fun <T> lateInit() = null as T
