52c0b5cd85c191774e7a66786bcfd8c2ac6d5a3b
WonJongWoon <delphi3228@pukyong.ac.kr>
2022-03-13 17:01:19 +0900

    feat: Add UserRepository

---
+++ src/main/kotlin/com/realworld/user/adapter/out/persistence/UserMapper.kt
@@ -0,0 +1,9 @@
+package com.realworld.user.adapter.out.persistence
+
+import com.realworld.user.application.port.`in`.RegistrationCommand
+
+class UserMapper {
+    fun mapToUser(registrationCommand: RegistrationCommand) {
+        TODO()
+    }
+}
