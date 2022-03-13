abca195fee737eb4dc3c176d8cca0d2326a4de48
WonJongWoon <delphi3228@pukyong.ac.kr>
2022-03-13 17:18:31 +0900

    feat: Add UserManagementController

---
+++ src/main/kotlin/com/realworld/user/adapter/in/web/UserManagementController.kt
@@ -0,0 +1,26 @@
+package com.realworld.user.adapter.`in`.web
+
+import com.realworld.user.application.port.`in`.RegistrationCommand
+import com.realworld.user.application.port.`in`.UserManagementUseCase
+import org.springframework.http.MediaType
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping(
+    path = ["/api"],
+    produces = [MediaType.APPLICATION_JSON_VALUE],
+    consumes = [MediaType.APPLICATION_JSON_VALUE]
+)
+class UserManagementController(
+    private val userManagementUseCase: UserManagementUseCase
+) {
+
+    @PostMapping(path = ["/users/"])
+    fun register(@RequestBody registrationCommand: RegistrationCommand) {
+        println("UserManagementController.register")
+        userManagementUseCase.register(registrationCommand)
+    }
+}
--- src/main/kotlin/com/realworld/user/application/port/in/UserManagementUseCase.kt
+++ src/main/kotlin/com/realworld/user/application/port/in/UserManagementUseCase.kt
@@ -1,7 +1,5 @@
 package com.realworld.user.application.port.`in`
 
-import com.realworld.user.domain.User
-
 interface UserManagementUseCase {
-    fun register(requestDto: RegistrationCommand): User
+    fun register(requestDto: RegistrationCommand)
 }
