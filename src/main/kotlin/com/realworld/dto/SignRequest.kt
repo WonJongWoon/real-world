8f611c1d8b0b597f0c366acfa8d4bb267afa0e87
WonJongWoon <delphi3228@pukyong.ac.kr>
2022-03-27 15:52:05 +0900

    feat: 잭슨 역직렬화 설정 추가

--- src/main/kotlin/com/realworld/dto/SignRequest.kt
+++ src/main/kotlin/com/realworld/dto/SignRequest.kt
@@ -1,16 +1,15 @@
 package com.realworld.dto
 
-import com.fasterxml.jackson.annotation.JsonTypeInfo
-import com.fasterxml.jackson.annotation.JsonTypeName
+import com.fasterxml.jackson.annotation.JsonRootName
 
-@JsonTypeName("user")
-@JsonTypeInfo(include = JsonTypeInfo.As.WRAPPER_OBJECT, use = JsonTypeInfo.Id.NAME)
 sealed class SignRequest {
+    @JsonRootName("user")
     data class In(
         val email: String,
         val password: String,
     )
 
+    @JsonRootName("user")
     data class Up(
         val username: String,
         val email: String,
