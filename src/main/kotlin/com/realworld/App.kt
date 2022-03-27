ec13231336aed12f57f0392ea42c41918f69d461
WonJongWoon <delphi3228@pukyong.ac.kr>
2022-03-27 17:31:54 +0900

    feat: BaseEntity 추가

--- src/main/kotlin/com/realworld/App.kt
+++ src/main/kotlin/com/realworld/App.kt
@@ -3,9 +3,11 @@ package com.realworld
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan
 import org.springframework.boot.runApplication
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing
 
 @SpringBootApplication
 @ConfigurationPropertiesScan
+@EnableJpaAuditing
 class RealWorldApplication
 
 fun main(args: Array<String>) {
---
+++ src/main/kotlin/com/realworld/common/BaseEntity.kt
@@ -0,0 +1,42 @@
+package com.realworld.common
+
+import org.springframework.data.annotation.CreatedDate
+import org.springframework.data.annotation.LastModifiedDate
+import org.springframework.data.jpa.domain.support.AuditingEntityListener
+import java.io.Serializable
+import java.time.LocalDateTime
+import javax.persistence.*
+import javax.persistence.GenerationType.IDENTITY
+
+@EntityListeners(AuditingEntityListener::class)
+@MappedSuperclass
+abstract class BaseEntity : Serializable {
+    @Id
+    @GeneratedValue(strategy = IDENTITY)
+    val id: Long? = lateinit()
+
+    @Column(updatable = false, nullable = false)
+    @CreatedDate
+    var createdAt: LocalDateTime = lateinit()
+    private set
+
+    @Column(nullable = false)
+    @LastModifiedDate
+    var modifiedAt: LocalDateTime = lateinit()
+    private set
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+        other as BaseEntity
+        if (id != other.id) return false
+        return true
+    }
+
+    override fun hashCode(): Int {
+        return id?.hashCode() ?: 0
+    }
+}
+
+@Suppress("UNCHECKED_CAST")
+fun <T> lateinit() = null as T
