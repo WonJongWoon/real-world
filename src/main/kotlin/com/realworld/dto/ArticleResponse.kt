023289a0d118f6000a67736b3f17d236746e0350
WonJongWoon <delphi3228@pukyong.ac.kr>
2022-03-27 15:08:35 +0900

    feat: 게시글 정보 객체 추가

---
+++ src/main/kotlin/com/realworld/dto/ArticleResponse.kt
@@ -0,0 +1,28 @@
+package com.realworld.dto
+
+import com.fasterxml.jackson.annotation.JsonTypeInfo
+import com.fasterxml.jackson.annotation.JsonTypeName
+import java.time.LocalDateTime
+
+@JsonTypeName("article")
+@JsonTypeInfo(include = JsonTypeInfo.As.WRAPPER_OBJECT, use = JsonTypeInfo.Id.NAME)
+data class ArticleResponse(
+    val slug: String,
+    val title: String,
+    val description: String,
+    val body: String,
+    val tagList: List<String>,
+    val createdAt: LocalDateTime,
+    val updatedAt: LocalDateTime,
+    val favorited: Boolean,
+    val favoritesCount: Long,
+    val author: Author
+) {
+
+    data class Author(
+        val userName: String,
+        val bio: String,
+        val image: String?,
+        val following: Boolean
+    )
+}
