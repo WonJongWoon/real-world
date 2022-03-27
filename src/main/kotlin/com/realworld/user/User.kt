397b586dd2a52a9484a714125a87644f63841977
WonJongWoon <delphi3228@pukyong.ac.kr>
2022-03-27 17:33:13 +0900

    feat: User Entity 추가

---
+++ src/main/kotlin/com/realworld/user/User.kt
@@ -0,0 +1,24 @@
+package com.realworld.user
+
+import com.realworld.common.BaseEntity
+import javax.persistence.Column
+import javax.persistence.Entity
+import javax.persistence.Table
+
+@Table(name = "USERS")
+@Entity
+class User(
+
+    @Column(nullable = false)
+    val email: String,
+
+    @Column(nullable = false)
+    val username: String,
+
+    @Column(nullable = false)
+    val bio: String,
+
+    @Column(nullable = true)
+    val image: String?
+) : BaseEntity()
+
